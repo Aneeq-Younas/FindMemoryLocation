4cc6e8c292416cbe259364c8356af30ce74cc58e
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-23 05:50:34 -0700

    Add files via upload

---
+++ lstdic.py
@@ -0,0 +1,17 @@
+
+list1 = [13,1231,42,324,2123,131,423,41,423,4,23,31423,4,235,24,1423,42,4212,4,1]
+
+list2 = ["python ", "Java ", "JavaScript ", "Rubby ", "Swift ", "C# ", "C++ " ]
+
+dict1 = {"Name": "Aneeq Younas ", "Age": 23, "Language": "Python "}
+
+dict2 = {"Name": "John ", "Age": 26, "Language": "JavaScrpt "}
+
+print("The Memory location of List1 is (Integers)   :\n ", hex(id(list1)))
+
+print("The Memory location of List2 is              :\n ", hex(id(list2)))
+
+print("The Memory location of Dictionary1 is        :\n ", hex(id(dict1)))
+
+print ("The Memory location of Dictionary2 is        :\n ", hex(id(len(dict2))))
+
